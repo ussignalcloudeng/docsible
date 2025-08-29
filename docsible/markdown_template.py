83ec7ad43608a56b8589be216b7300e7bf045a8b
Alex Dietrich <adietrich@ussignal.com>
2025-08-29 09:44:08 -0400

    fix spacing

--- docsible/markdown_template.py
+++ docsible/markdown_template.py
@@ -78,8 +78,8 @@ Description: {{ role.meta.galaxy_info.description or 'Not available.' }}
   {% if details.description is iterable and (details.description is not string and details.description is not mapping) -%}
   {{ indent }}  - **Description**: 
   {% for details_desc in details.description -%}
-      {{ indent }} {{ indent }} {{ indent }} - {{ details_desc }}
-  {% endfor -%}
+      {{ indent }} - {{ details_desc }}
+  {%- endfor %}
   {% else %}
   {{ indent }}  - **Description**: {{ details.description | default('No description provided') }}
   {%- endif %}
